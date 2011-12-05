6f00d9ac768c1df44c2bebf4c703d8d822ca3e4d
renctan <renctan@gmail.com>
2011-12-05 15:25:34 -0500

    rearranged dom heirarchies

--- src/main_ui.js
+++ src/main_ui.js
@@ -10,6 +10,8 @@ goog.require('goog.events.EventType');
 goog.require('goog.ui.Button');
 goog.require('goog.ui.FlatButtonRenderer');
 
+var ADD_PLAYER_MSG = 'Add players then press the start to begin playing.';
+
 /**
  * @param {Node} node The <b>div<\b> element to attach this UI to.
  * @constructor
@@ -17,32 +19,40 @@ goog.require('goog.ui.FlatButtonRenderer');
 drecco.sudokill.MainUI = function(node) {
   var self = this;
   this._statusBarDom = goog.dom.createDom('div', { id: 'status-bar' });
+  goog.dom.setTextContent(this._statusBarDom, ADD_PLAYER_MSG);
+  goog.dom.appendChild(node, this._statusBarDom);
+
   this._gameState = null;
   this._boardDom = goog.dom.createDom('div', { id: 'sudokill-board' });
   this._isPlaying = false;
 
-  goog.dom.appendChild(node, this._statusBarDom);
-  goog.dom.appendChild(node, this._boardDom);
+  drecco.sudokill.BoardUI.renderEmptyBoard(this._boardDom);
 
-  this._playerList = new drecco.sudokill.PlayerListUI(node);
+  var gameSideBarDom = goog.dom.createDom('div', { 'id': 'sudokill-sidebar' });
+  this._playerList = new drecco.sudokill.PlayerListUI(gameSideBarDom);
 
   var startGameDom = goog.dom.createDom('div', { 'class': 'start-game-btn' });
   this._startGameBtn = new goog.ui.Button('Start Game',
     goog.ui.FlatButtonRenderer.getInstance());
   this._startGameBtn.render(startGameDom);
-  goog.dom.appendChild(node, startGameDom);
+  goog.dom.appendChild(gameSideBarDom, startGameDom);
+
+  var boardAndSideDom = goog.dom.createDom('div', { id: 'board-and-sidebar' },
+    this._boardDom, gameSideBarDom);
+
+  goog.dom.appendChild(node, boardAndSideDom);
 
   var handleStartBtn = function(e) {
     var thisRef = self;
 
     if (thisRef._isPlaying) {
-      goog.dom.setTextContent(startGameDom, 'Start Game');
+      goog.dom.setTextContent(thisRef._startGameBtn.getElement(), 'Start Game');
       thisRef._playerList.enable();
       thisRef._disposeBoard();
       goog.dom.setTextContent(thisRef._statusBar, '');
     }
     else {
-      goog.dom.setTextContent(startGameDom, 'New Game');
+      goog.dom.setTextContent(thisRef._startGameBtn.getElement(), 'New Game');
       thisRef._playerList.disable();
       thisRef._createBoard(15);
       thisRef._startGameBtn.setEnabled(false);
@@ -74,6 +84,7 @@ drecco.sudokill.MainUI.prototype._handleGameOver = function(e) {
  */
 drecco.sudokill.MainUI.prototype._disposeBoard = function() {
   goog.dom.removeChildren(this._boardDom);
+  drecco.sudokill.BoardUI.renderEmptyBoard(this._boardDom);
   this._gameState = null;
 };
 
@@ -84,6 +95,7 @@ drecco.sudokill.MainUI.prototype._disposeBoard = function() {
  * @private
  */
 drecco.sudokill.MainUI.prototype._createBoard = function(filledCell) {
+  goog.dom.removeChildren(this._boardDom);
   this._gameState = new drecco.sudokill.BoardUI(filledCell,
     this._playerList, this._boardDom);
   goog.events.listen(this._gameState, drecco.sudokill.EventType.GAME_OVER,
