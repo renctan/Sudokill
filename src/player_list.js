20037ca5965bf5afe84b0f795dfd1530d7d7d9d2
renctan <renctan@gmail.com>
2011-12-05 21:12:25 -0500

    added player list model

---
+++ src/player_list.js
@@ -0,0 +1,77 @@
+goog.provide('drecco.sudokill.PlayerList');
+
+/**
+ * Creates a container to store a list of players that can be cycled through in
+ * a round robin fashion.
+ * 
+ * @param {Array.<drecco.sudokill.Player>} players The list of players.
+ * @constructor
+ */
+drecco.sudokill.PlayerList = function(players) {
+  this._players = players;
+  this._currIdx = 0;
+};
+
+/**
+ * @return {drecco.sudokill.Player} the next player.
+ */
+drecco.sudokill.PlayerList.prototype.next = function() {
+  var size = this._players.length;
+  var next = null;
+
+  if (size > 0) {
+    next = this._players[this._currIdx++];
+
+    if (this._currIdx >= size) {
+      this._currIdx = this._currIdx % size;  
+    }
+  }
+
+  return next;
+};
+
+/**
+ * @return {drecco.sudokill.Player} the current player.
+ */
+drecco.sudokill.PlayerList.prototype.getCurrentPlayer = function() {
+  var player = null;
+
+  if (this._players.length > 0) {
+    player = this._players[this._currIdx];
+  }
+
+  return player;
+};
+
+/**
+ * @return {number} the number of players.
+ */
+drecco.sudokill.PlayerList.prototype.playerCount = function() {
+  return this._players.length;
+};
+
+/**
+ * @return {Array.<string>} an array of all players names except the current player.
+ */
+drecco.sudokill.PlayerList.prototype.getAllExceptCurrent = function() {
+  var players = [];
+  var size = this._players.length;
+  var currIdx = this._currIdx + 1;
+
+  if (currIdx >= size) {
+    currIdx = currIdx % size;
+  }
+
+  if (size > 1) {
+    while (currIdx != this._currIdx) {
+      players.push(this._players[currIdx++].name());
+
+      if (currIdx >= size) {
+        currIdx = currIdx % size;
+      }
+    }
+  }
+  
+  return players;
+};
+
--- test/alltests.js
+++ test/alltests.js
@@ -1,4 +1,5 @@
 var _allTests = [
   'js/board_test.html',
-  'js/board_factory_test.html'
+  'js/board_factory_test.html',
+  'js/player_list_test.html'
 ];
